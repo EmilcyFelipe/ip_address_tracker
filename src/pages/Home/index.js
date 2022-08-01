6d8d66bc4b11f6b4112573d1d6bff88e34867b48
Felipe de Paula <emilcyfelipe@gmail.com>
2022-07-31 22:14:13 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import bg from "../../assets/pattern-bg.png";
+
+import { Container, Bg } from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Bg />
+    </Container>
+  );
+}
